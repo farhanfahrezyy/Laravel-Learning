0df8eb117778a035c4a4b866f7d99502087daa08
farhanfahrezyy <farhanfahrezy04@gmail.com>
2022-09-07 15:12:21 +0700

    commit 1

---
+++ database/factories/UsersFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Users;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class UsersFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'username' => 'Farhanfahrezyy',
+            'password' => bcrypt('testing1234')
+        ];
+    }
+}
